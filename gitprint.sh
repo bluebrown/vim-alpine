41d71935a756afcab673aa221078bce1fefc03a2
bluebrown <nico-braun@live.de>
2019-01-13 19:42:33 +0100

    gitprint for docker added

---
+++ gitprint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git config --global --unset-all user.name
+git config --global user.name "bluebrown"
+git config --global --unset-all user.email
+git config --global user.email "nico-braun@live.de"
